e0e7798e8d3babe209c54115b8e61aacdea10b94
Carys <77974075+carys23@users.noreply.github.com>
2021-10-21 15:27:31 +0100

    Created a random number
    Created a random number between 1 and 10

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+using System;
+
+namespace c
+
+{
+    class Program
+    {
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+        for (int i = 0; i < 1; i ++)
+        {
+            Console.WriteLine(random.Next(101));
+        }
+        Console.ReadLine();
+    }
+}
+}
+
