790e188f86275db41c9418da5664b42fd4305e0a
Carys <77974075+carys23@users.noreply.github.com>
2021-11-02 15:14:26 +0000

    Completed binary search
    + Added generate list to make an array list to find the index so can add count
    + Got the binary search to amend the middle number each time by checking with the high and low

--- Program.cs
+++ Program.cs
@@ -6,71 +6,82 @@ namespace SerachingAlgorithm
 {
     class BinaryAndLinear
     {
-    const int maxValue = 1000000;
+    const int ArraySize = 1000000;
+    const int maxValue = 10000000;
     static void Main(string[] args)
     {
         Random random = new Random();
         int randomNumber = random.Next(maxValue);
-        LinearSearch(randomNumber);
-        BinarySearch(randomNumber);
+        int [] ListOfValues = GenerateList(ArraySize, maxValue);
+        LinearSearch(randomNumber, ListOfValues);
+        BinarySearch(randomNumber, ListOfValues);
+
     }
 
+    static int[] GenerateList(int NumberValues, int maxValue)
+    {
+        int[] List = new int[NumberValues];
+        Random random = new Random();
+        for (int i = 0; i < NumberValues; i++)
+        {
+            List[i] = random.Next(maxValue);
+        }
+        Array.Sort(List);
+        return List;
+    }
 
-
-    static void BinarySearch(int randomNumber)
-
+    static void BinarySearch(int ValueToFind, int[] ValuesToCheck)
     /// The binary search will split the number in the middle to find the random number
     /// and will continue to do this until it finds the number. The valueChecked is a counter
     /// to count how many times to took to find the random number.
     {
-        // int valueChecked = -1;
-        int currentValue = 0;
-        int mid = (currentValue + maxValue) / 2;
+        int low = 0;
+        int high = ArraySize;
+        int count = 0;
 
-        Console.WriteLine("Mid is " + mid );
+        while (low < high)
+        {
+            count++;
+            int mid = (low + high) / 2;  /// Find the middle number
 
-        if (mid == randomNumber)
+            if (ValueToFind == ValuesToCheck[mid])
             {
-                Console.WriteLine("Found number " + randomNumber + " is was " + mid);
-                return;
-
-            }
-
-        else
-            if (mid > randomNumber)
-            {
-                int NewValue = mid;
-                Console.WriteLine("Max value is now " + NewValue);
+                Console.WriteLine("Found number " + ValueToFind + " after " + count + " counts");
                 return;
             }
 
+            if ( ValueToFind < ValuesToCheck[mid])
+            {
+                high = mid -1;
+            }
+            else
+            {
+                low = mid +1;
+            }
+        }
+        Console.WriteLine("Couldn't find value " + ValueToFind + " it took " + count + " counts to try to find.");
     }
 
 
 
-    static void LinearSearch(int randomNumber)
+    static void LinearSearch(int ValueToFind, int[] ValuesToCheck)
     /// The linear search will search for the random numbder created search from 1 and incrementing each time
     /// until it finds the random number. The valueChecked is a counter to see how times to took to find the number.
 
     {
-
-        int valueChecked = -1;
-
-        for (int currentValue = 0; currentValue <= maxValue; currentValue++)
+        for (int i = 0; i < ArraySize; i++)
 
         {
-            valueChecked += 1;
+            if (ValuesToCheck[i] == ValueToFind)
 
-            if (currentValue == randomNumber)
             {
-                Console.WriteLine("Found number " + randomNumber);
-                Console.WriteLine("It was found in " + valueChecked + " counts.");
+                Console.WriteLine("It was found in " + (i +1) + " counts.");
                 return;
-
             }
+
         }
 
-        Console.WriteLine(randomNumber + "Was not in list");
+        Console.WriteLine(ValueToFind + " was not in list");
     }
 
     }
